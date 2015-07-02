f038d9772a0622274e44f33636f5ec9f74126b04
Seabook0303 <k.t.seabook0303@gmail.com>
2015-07-03 01:00:43 +0900

    add kml
    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('Source/20150704KML.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/20150704test/cesium-starter-app-master/Source/20150704KML.kml'));
