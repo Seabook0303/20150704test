8fcf20ee3105fd6e80db27c5c98cce1cb1d6a409
Seabook0303 <k.t.seabook0303@gmail.com>
2015-07-03 17:23:45 +0900

    test
    test upload

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,5 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/20150704test/cesium-starter-app-master/Source/20150704KML.kml'));
+
+viewer.dataSources.add(Cesium.CzmlDataSource.load('http://seabook0303.github.io/20150704test/cesium-starter-app-master/Source/okinawatimes_shogen.czml'));
