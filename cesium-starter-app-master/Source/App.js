385eed63fd8145d8f5e48c17f8c4b8367f8781df
Seabook0303 <k.t.seabook0303@gmail.com>
2015-07-04 00:27:37 +0900

    test
    test upload

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,5 +1,6 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
+viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
+
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/20150704test/cesium-starter-app-master/Source/20150704KML.kml'));
 
-viewer.dataSources.add(Cesium.CzmlDataSource.load('http://seabook0303.github.io/20150704test/cesium-starter-app-master/Source/okinawatimes_shogen.czml'));
