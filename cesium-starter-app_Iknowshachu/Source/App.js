d8604394ff5199643d0b1c85a2178faeb2f6c86e
Seabook0303 <k.t.seabook0303@gmail.com>
2015-07-04 03:16:12 +0900

    testkml
    test

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,2 +1,2 @@
-var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+var cesiumtest = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/20150704test/cesium-starter-app_Iknowshachu/Source/20150704KML.kml'));
